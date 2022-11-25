a0a5050fd68d3204a4cca6d8860a5db49d9212ed
noot <elizabethjbinks@gmail.com>
2022-11-25 12:38:48 -0500

    implement prove

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,10 @@
-use ff::{Field, PrimeField};
-use group::{Group, GroupOps, GroupOpsOwned, ScalarMul, ScalarMulOwned};
-use num_bigint_dig::{RandBigInt, ToBigInt, ToBigUint};
+// use ff::{Field, PrimeField};
+use group::{GroupOps, GroupOpsOwned, ScalarMul, ScalarMulOwned};
+use num_bigint_dig::{BigUint, RandBigInt, ToBigUint};
 use rand::{self, CryptoRng};
+use sha2::{Digest, Sha256};
 use std::marker::PhantomData;
 use std::ops::{Add, Mul, Neg, Sub};
-
-use elliptic_curve::generic_array::GenericArray;
 
 /// BITLEN_CHALLENGE represents the bitlength of the challenge.
 const BITLEN_CHALLENGE: usize = 128;
@@ -17,21 +16,21 @@ const BITLEN_WITNESS: usize = 112;
 /// Higher values mean the protocol is less likely to fail.
 const BITLEN_FAILURE: usize = 12;
 
-pub struct DLEqProver<Gp: MyGroup, Gq: MyGroup> {
+pub struct DLEqProver<Gp: DLEqGroup, Gq: DLEqGroup> {
     phantom_p: PhantomData<Gp>,
     phantom_q: PhantomData<Gq>,
 }
 
-pub struct DLEqProof<Gp: MyGroup, Gq: MyGroup> {
+pub struct DLEqProof<Gp: DLEqGroup, Gq: DLEqGroup> {
     // TODO: add range proof
     pub Kp: Gp,
     pub Kq: Gq,
-    // pub z: Gp::Scalar, // actually a scalar?
-    // pub sp: Gp::Scalar,
-    // pub sq: Gq::Scalar,
+    pub z: BigUint,
+    pub sp: Gp::Scalar,
+    pub sq: Gq::Scalar,
 }
 
-pub trait MyGroup:
+pub trait DLEqGroup:
     Clone
     + Copy
     + Eq
@@ -45,21 +44,22 @@ pub trait MyGroup:
     + ScalarMul<Self::Scalar>
     + ScalarMulOwned<Self::Scalar>
 {
-    type Scalar: MyField;
+    type Scalar: DLEqField;
     /// Basepoint
     fn generator() -> Self;
     /// Alt basepoint
     fn alt_generator() -> Self;
+    fn to_bytes_be(&self) -> Vec<u8>;
 }
 
-pub trait MyField:
+pub trait DLEqField:
     Sized + Eq + Add<Output = Self> + Sub<Output = Self> + Mul<Output = Self> + Neg<Output = Self>
 {
     fn from_be_bytes(_: &[u8]) -> Self;
     fn random<R: CryptoRng>(_: R) -> Self;
 }
 
-impl<Gp: MyGroup, Gq: MyGroup> DLEqProver<Gp, Gq> {
+impl<Gp: DLEqGroup, Gq: DLEqGroup> DLEqProver<Gp, Gq> {
     pub fn new() -> DLEqProver<Gp, Gq> {
         DLEqProver::<Gp, Gq> {
             phantom_p: PhantomData,
@@ -67,8 +67,7 @@ impl<Gp: MyGroup, Gq: MyGroup> DLEqProver<Gp, Gq> {
         }
     }
 
-    // x's type should be of the field with a smaller order.
-    pub fn prove<P: PrimeField, F: Field>(x: &P) -> DLEqProof<Gp, Gq> {
+    pub fn prove(x: &[u8]) -> DLEqProof<Gp, Gq> {
         let one = 1_i32.to_biguint().unwrap();
         let two = 2_i32.to_biguint().unwrap();
         let pow = (BITLEN_CHALLENGE + BITLEN_WITNESS + BITLEN_FAILURE)
@@ -85,15 +84,44 @@ impl<Gp: MyGroup, Gq: MyGroup> DLEqProver<Gp, Gq> {
         let kp = Gp::Scalar::from_be_bytes(&k);
         let kq = Gq::Scalar::from_be_bytes(&k);
 
-        let Kp = Gp::generator() * kp + Gp::alt_generator() * tp;
-        let Kq = Gq::generator() * kq + Gq::alt_generator() * tq;
+        let Kp = Gp::generator() * kp + Gp::alt_generator() * &tp;
+        let Kq = Gq::generator() * kq + Gq::alt_generator() * &tq;
+
+        let xp = Gp::Scalar::from_be_bytes(x);
+        let xq = Gq::Scalar::from_be_bytes(x);
+        let Yp = Gp::generator() * &xp;
+        let Yq = Gq::generator() * &xq;
+
+        let mut hasher = Sha256::new();
+        hasher.update(Gp::generator().to_bytes_be());
+        hasher.update(Gq::generator().to_bytes_be());
+        hasher.update(Gp::alt_generator().to_bytes_be());
+        hasher.update(Gq::alt_generator().to_bytes_be());
+        hasher.update(Kp.to_bytes_be());
+        hasher.update(Kq.to_bytes_be());
+        hasher.update(Yp.to_bytes_be());
+        hasher.update(Yq.to_bytes_be());
+        let res = hasher.finalize();
+
+        let c_unreduced = BigUint::from_bytes_be(&res);
+        let bitlen_commitment = BITLEN_CHALLENGE.to_biguint().unwrap();
+        let commitment_modulus = two.modpow(&bitlen_commitment, &one);
+        let c = c_unreduced.modpow(&one, &commitment_modulus);
+
+        let x_biguint = BigUint::from_bytes_be(x);
+        let z = k_biguint + &c * x_biguint;
+
+        let cp = Gp::Scalar::from_be_bytes(&c.to_bytes_be());
+        let cq = Gq::Scalar::from_be_bytes(&c.to_bytes_be());
+        let sp = tp + cp * xp;
+        let sq = tq + cq * xq;
 
         DLEqProof {
             Kp: Kp,
             Kq: Kq,
-            // z: (),
-            // sp: (),
-            // sq: (),
+            z: z,
+            sp: sp,
+            sq: sq,
         }
     }
 }
